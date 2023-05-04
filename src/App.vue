bd08ecf3b78187dd56a1df2efa1a9c2f2ee2e876
wangquanming <mingo.email@qq.com>
2023-05-04 14:37:26 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="headerWrapper">
+    <logo> Mingo Wang </logo>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/photo">Photo</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="postcss">
+.headerWrapper {
+  @apply flex justify-between items-center text-lg pt-4 pr-8 pl-8;
+}
+nav {
+  @apply flex justify-end gap-2;
+}
+a {
+  @apply hover:underline;
+}
+</style>
